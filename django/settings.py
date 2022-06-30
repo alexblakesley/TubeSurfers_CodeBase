861452c2d0b930f527e490ccb1102d8d8067386d
Alex Blakesley <alexander.blakesley@ucl.ac.uk>
2022-06-30 18:12:26 +0100

    deployment tests

--- django/settings.py
+++ django/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'fr!3by5i&ey^-9s^*gl$*1)uoxf3l441r2t5l$!((pg2j2l#=('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['TubeSurfersEnv.eba-pabcmmyh.eu-west-2.elasticbeanstalk.com']
 
 
 # Application definition
