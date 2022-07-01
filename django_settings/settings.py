fccd33930c6db8e8ede2ec8c4c620703960ca85f
Alex Blakesley <alexander.blakesley@ucl.ac.uk>
2022-07-01 14:10:34 +0100

    Deployment fixes

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -12,6 +12,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
