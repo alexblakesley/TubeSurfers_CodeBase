1d53079fad895be760fdf7d784c4da971f3a1b24
Jan Luca Kaestle <janluca.kaestle.18@ucl.ac.uk>
2022-07-04 15:41:50 +0100

    audio insertion

--- InsertData.py
+++ InsertData.py
@@ -4,13 +4,17 @@ import core.inserters.AudioInserter as AudioInserter
 import core.processors.AudioProcessor as AudioProcessor
 import os
 
+
+### Uncomment this to process raw audio. UNTESTED IN NEWEST VERSION!
+# AudioProcessor()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_settings.development_settings')
-# CoreInserter.InsertAll();
+CoreInserter.InsertAll();
 
-# for filename in os.listdir("data/audio/"):
-#     AudioInserter.Insert(filename)
+for filename in os.listdir("data/audio/"):
+    AudioInserter.Insert(filename)
 
-AudioProcessor.ProcessAudio()
+
 
 
 
