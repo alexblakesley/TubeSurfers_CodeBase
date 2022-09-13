e48cd4c2482c0b1b227421c9ecd104c161b3db55
Alex Blakesley <alexander.blakesley@ucl.ac.uk>
2022-09-13 20:01:26 +0100

    Further Update to deployment

--- django_settings/production_settings.py
+++ django_settings/production_settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'static'
 LOGIN_REDIRECT_URL = '../dev/'
 
 BASE_URL = "tubesurfers.co.uk"
